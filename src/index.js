9fc8d6ad6a8db64c98295470623a5a8339c2370f
dashdashzako <d@dashdashzako.net>
2020-06-08 12:08:21 +0200

    Improve JSDoc

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import assignWordEntry from './helpers/assign-word-entry';
  *
  * @param {string} text - The text to be turned to words
  * @param {number} [len=0] - Minimal length of the string
+ * @throws Will throw an error if first parameter is not a string
  */
 export default function countWords(text, len = 0) {
   if (typeof text !== 'string') {
-    throw Error('str should be a String');
+    throw Error('text should be a String');
   }
 
   return text
